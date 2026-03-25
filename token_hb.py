3b13f2788d2a648cfa1a5c5eff2e781aca0957fd
Habachi000 <habachioff@gmail.com>
2026-03-25 23:03:17 +0100

    Change server status message to 'Habachi'

--- token_hb.py
+++ token_hb.py
@@ -4,4 +4,4 @@
 FIREBASE_URL = "https://follower-hb-default-rtdb.firebaseio.com/amin.json"
 MY_SECRET = "Vem8760WElX5wFkflX9D0DsT9cznsPbGhrLseQtf"
 async def root():
-    return {"status": "ســـــرفر يعمـــل  "}
+    return {"status": " Habachi  "}
