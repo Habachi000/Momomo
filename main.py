acba90f7f55dbddfff2f7c1831e93fb7433634fd
Habachi000 <habachioff@gmail.com>
2026-03-26 00:11:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,26 @@
-import requests
 import os
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-import  token_hb
+# --- هنا عيطنا على الملف الجديد والفانكشن اللي فيه ---
+from firebase_service import send_to_firebase 
+
 app = FastAPI()
 
 class UserPayload(BaseModel):
     text: str
-    
+
 @app.get("/")
-async def Test_GET():
+async def test_get():
+    import token_hb # حيت استعملتيه هنا
     return token_hb.root()
 
 @app.post("/update-user")
 async def update_firebase_user(data: UserPayload):
-    try:
-        final_url = f"{token_hb.FIREBASE_URL}?auth={token_hb.MY_SECRET}"
-        
-        payload = {
-            "user_content": data.text,
-            "timestamp": "2026-03-25" 
-        }
-        
-        response = requests.post(final_url, json=payload, timeout=10)
-        generated_id = response.json().get("name")
-        
-        return {
-            "status": "success", 
-            "firebase_id": generated_id
-        }
-    except Exception as e:
-        return {"status": "error", "message": str(e)}
-        return {
-            "status": "success", 
-            "firebase_id": generated_id
-        }
-    except Exception as e:
-        return {"status": "error", "message": str(e)}
+    # عيطنا على الخدمة من الملف الآخر وصيفطنا ليها غير النص
+    result = send_to_firebase(data.text)
+    return result
+
+if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    # كنخدمو بـ 0.0.0.0 باش السيرفر يقبل اتصالات من "برا"
     uvicorn.run(app, host="0.0.0.0", port=port)
