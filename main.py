41d549e937b32c28f6127ff32ffe4d2466483e9d
Habachi000 <habachioff@gmail.com>
2026-03-25 17:51:52 +0100

    Update server status message in root endpoint

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ MY_SECRET = "Vem8760WElX5wFkflX9D0DsT9cznsPbGhrLseQtf"
 
 @app.get("/")
 async def root():
-    return {"status": "ســـــرفر يعمـــل ✨💕 HabaChi HB"}
+    return {"status": "ســـــرفر يعمـــل ✨💕 HabaChi "}
 
 @app.post("/update-user")
 async def update_firebase_user(data: UserPayload):
