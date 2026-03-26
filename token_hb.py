c334fdd6fcf092ea610fc443cddcc250e5d9e6c6
Habachi000 <habachioff@gmail.com>
2026-03-26 12:48:33 +0100

    Update token_hb.py

--- token_hb.py
+++ token_hb.py
@@ -1,7 +1,7 @@
 
 
 
-FIREBASE_URL = "https://follower-hb-default-rtdb.firebaseio.com/xxxxxxx.json"
+FIREBASE_URL = "https://follower-hb-default-rtdb.firebaseio.com/.json"
 MY_SECRET = "Vem8760WElX5wFkflX9D0DsT9cznsPbGhrLseQtf"
 async def root():
     return {"status": " Habachi  "}
