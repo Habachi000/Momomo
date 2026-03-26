e03e0f961172eaa6f12d052aeaf8ee4d28e2be07
Habachi000 <habachioff@gmail.com>
2026-03-26 13:51:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,42 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-# --- هنا عيطنا على الملف الجديد والفانكشن اللي فيه ---
 from firebase_service import send_to_firebase 
+from UpdateDataKey_HB import  update_data_key
 
 app = FastAPI()
 
 class UserPayload(BaseModel):
     text: str
+    
+class PlusCoinsPayload(BaseModel):
+    text: str
+    
+class UbdatValue_FromKey(BaseModel):
+    text: str
+    id:str
+    
 
 @app.get("/")
 async def test_get():
-    import token_hb # حيت استعملتيه هنا
+    import token_hb 
     return token_hb.root()
 
 @app.post("/update-user")
 async def update_firebase_user(data: UserPayload):
-    # عيطنا على الخدمة من الملف الآخر وصيفطنا ليها غير النص
     result = send_to_firebase(data.text)
+    return result
+
+@app.post("/Update_DataKey")
+async def UbdatData (data: UbdatValue_FromKey):
+    result = update_data_key(data.text,data.id)
+    return result
+
+
+
+@app.post("/PlusCoins-user")
+async def Delet(data: UserPayload):
+    result = update_data_key(data.text,data.id)
     return result
 
 if __name__ == "__main__":
